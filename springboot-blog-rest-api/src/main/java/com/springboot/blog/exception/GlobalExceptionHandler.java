6bc4d66edce179bd6e4bfd3bca2eb59dda08e3ef
sharmila2901 <48612055+sharmila2901@users.noreply.github.com>
2021-12-31 16:46:24 +0530

    Add files via upload

---
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.springboot.blog.exception;
+
+import com.springboot.blog.Payload.ErrorDetails;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    //handle global exception
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDetails> handleResourceNotfoundException (ResourceNotFoundException exception,
+                                                                         WebRequest webRequest){
+        ErrorDetails errorDetails =new ErrorDetails(new Date(),exception.getMessage(),
+                webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> handleGlobalException (Exception exception,
+                                                                         WebRequest webRequest){
+        ErrorDetails errorDetails =new ErrorDetails(new Date(),exception.getMessage(),
+                webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
